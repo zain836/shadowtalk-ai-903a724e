2f71919f48fc971166dbd9a2b516c4397c889a42
zaima2523 <zaima2523@gmail.com>
2025-12-18 11:28:59 +0000

    feat: Complete all tasks and implement monetization script

---
+++ src/integrations/firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnWVCSWxvVfIafWISbvOxYfFfPb8-P0Lg",
+  authDomain: "genesis-agent-i6qqq.firebaseapp.com",
+  projectId: "genesis-agent-i6qqq",
+  storageBucket: "genesis-agent-i6qqq.firebasestorage.app",
+  messagingSenderId: "35140567703",
+  appId: "1:35140567703:web:dcecb5d0c59a7f42339fd2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
